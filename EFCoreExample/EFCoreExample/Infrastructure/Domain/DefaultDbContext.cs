c7e10ada7f292c539d3e0eb4fab6a3137a4ed338
Goshen Jimenez <gjimenez@abcidee.com>
2022-10-08 10:43:30 +0800

    Entity Framework

---
+++ EFCoreExample/EFCoreExample/Infrastructure/Domain/DefaultDbContext.cs
@@ -0,0 +1,45 @@
+﻿using EFCoreExample.Infrastructure.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreExample.Infrastructure.Domain
+{
+    public class DefaultDbContext : DbContext
+    {
+        public DefaultDbContext(DbContextOptions<DefaultDbContext> options)
+          : base(options)
+        {
+        }
+        public DbSet<Role> Roles { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            List<Role> roles = new List<Role>();
+
+            roles.Add(new Role()
+            {
+                Id = Guid.Parse("00965ecf-acae-46fe-8775-d7834b07fd96"),
+                Name = "Staff",
+                Description = "People who work in school but not teaching",
+                Abbreviation = "Stf"
+            });
+
+            roles.Add(new Role()
+            {
+                Id = Guid.Parse("7ce68d5c-5b65-495a-8a63-14aeb48da87d"),
+                Name = "Faculty",
+                Description = "People who teach in school",
+                Abbreviation = "Fct"
+            });
+
+
+            roles.Add(new Role()
+            {
+                Id = Guid.Parse("1fb7085a-762f-440c-87de-59f75f85e935"),
+                Name = "Student",
+                Description = "People who learn in school",
+                Abbreviation = "Std"
+            });
+
+            modelBuilder.Entity<Role>().HasData(roles);
+        }
+    }
+}
---
+++ EFCoreExample/EFCoreExample/Migrations/20221008023635_init.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreExample.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Abbreviation = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Abbreviation", "Description", "Name" },
+                values: new object[] { new Guid("00965ecf-acae-46fe-8775-d7834b07fd96"), "Stf", "People who work in school but not teaching", "Staff" });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Abbreviation", "Description", "Name" },
+                values: new object[] { new Guid("1fb7085a-762f-440c-87de-59f75f85e935"), "Std", "People who learn in school", "Student" });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Abbreviation", "Description", "Name" },
+                values: new object[] { new Guid("7ce68d5c-5b65-495a-8a63-14aeb48da87d"), "Fct", "People who teach in school", "Faculty" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
